b8665d69fc6288fb529428a42d47fd90726e204b
Lanxin Wang <lanxin.wang@thoughtworks.com>
2022-11-21 22:43:41 +0800

    set up a simple apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,29 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
 const typeDefs = `
   type Todo {
-    title: String
-    author: String
+    _id: String
+    status: String
+    name: String
   }
 
-  case, the "Todos" query returns an array of zero or more Todos (defined above).
   type Query {
-    Todos: [Todo]
+    todos: [Todo]
   }
 `;
 
 const todos = [
     {
       _id: '0',
-      name: 'todo 1',
       status: 'active',
-      
+      name: 'todo 1',      
     },
     {
       _id: '1',
+      status: 'completed',
       name: 'todo 2',
-      status: 'completed'
-      
-    },
+    }
   ];
 
 const resolvers = {
