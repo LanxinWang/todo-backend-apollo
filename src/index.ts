7e36a0c823b3269ba62c5c76352e1fe4ff079472
Lanxin Wang <lanxin.wang@thoughtworks.com>
2022-11-21 22:35:32 +0800

    create a instance of ApolloServer

--- src/index.ts
+++ src/index.ts
@@ -36,3 +36,14 @@ const resolvers = {
       todos: () => todos,
     },
   };
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+  
+  console.log(`🚀  Server ready at: ${url}`);
