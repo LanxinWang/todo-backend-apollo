aff91727397ab4b1a79c0b36414b33e709c2dac6
Lanxin Wang <lanxin.wang@thoughtworks.com>
2022-11-22 15:20:39 +0800

    add mongoose todo schema

---
+++ src/schema/todoSchema.ts
@@ -0,0 +1,9 @@
+import * as mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+export const todoSchema = new Schema ({
+    _id: { type: Number },
+    status: { type: String },
+    name: { type: String },
+}, { versionKey: false})
+
