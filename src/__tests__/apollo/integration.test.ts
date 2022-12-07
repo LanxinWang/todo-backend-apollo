f86b3e0eaaf4208c29d8f02d6de5c08578c53c8b
Lanxin Wang <lanxin.wang@thoughtworks.com>
2022-12-07 10:52:39 +0800

    test add a todo mutation

--- src/__tests__/apollo/integration.test.ts
+++ src/__tests__/apollo/integration.test.ts
@@ -46,7 +46,51 @@ describe("resolvers", () => {
 
     expect(res.body.kind).toBe("single");
     if (res.body.kind === "single") {
+      expect(res.body.singleResult.errors).toEqual(undefined);
       expect(res.body.singleResult.data.todos).toEqual(mockTodos);
+    }
+  });
+
+  it("mutation add a todo", async () => {
+    const mockATodo: ITodo = {
+      _id: 0,
+      status: TODO_STATUS.ACTIVE,
+      name: "todo 0",
+    };
+    const ADD_A_TODO = gql`
+      mutation AddATodo($id: Int!, $status: String!, $name: String!) {
+        addATodo(_id: $id, status: $status, name: $name) {
+          _id
+          status
+          name
+        }
+      }
+    `;
+    const mockAddATodoResponse: ITodo = mockATodo;
+    todosAPI.addATodo = jest.fn(() => Promise.resolve(mockAddATodoResponse));
+
+    const res = await testServer.executeOperation(
+      {
+        query: ADD_A_TODO,
+        variables: {
+          id: mockATodo._id,
+          name: mockATodo.name,
+          status: mockATodo.status,
+        },
+      },
+      {
+        contextValue: {
+          dataSources: {
+            todosAPI,
+          },
+        },
+      }
+    );
+
+    expect(res.body.kind).toBe("single");
+    if (res.body.kind === "single") {
+      expect(res.body.singleResult.data.addATodo).toEqual(mockATodo);
+      expect(res.body.singleResult.errors).toEqual(undefined);
     }
   });
 });
