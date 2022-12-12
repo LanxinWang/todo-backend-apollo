bc2620baa0bf9b7586cb500850a9486e9f96dfc9
Lanxin Wang <lanxin.wang@thoughtworks.com>
2022-12-12 14:57:54 +0800

    add tests about database

--- src/__tests__/mongoose/index.test.ts
+++ src/__tests__/mongoose/index.test.ts
@@ -1,5 +1,60 @@
-describe("test", () => {
-  it("test", () => {
-    expect(1).toBe(1);
+import mongoose from "mongoose";
+import { TodoModel as Todo } from "../../mongoose/model/todoModel";
+import { clearDB, closeDB, connectDB } from "../utils/db";
+
+beforeAll(async () => {
+  await connectDB();
+});
+
+afterAll(async () => {
+  await closeDB();
+});
+
+afterEach(async () => {
+  await clearDB();
+});
+describe("Todo", () => {
+  it("should create a todo item successfully", async () => {
+    const validTodo = {
+      _id: 0,
+      status: "active",
+      name: "test 1",
+    };
+    const newTodo = new Todo(validTodo);
+    await newTodo.save();
+    expect(newTodo._id).toBeDefined();
+    expect(newTodo.name).toBe(validTodo.name);
+    expect(newTodo.status).toBe(validTodo.status);
   });
+
+  it("should throw error when create a todo item failed", async () => {
+    const invalidTodo = {
+      _id: 0,
+      status: "active",
+    };
+    try {
+      const newTodo = new Todo(invalidTodo);
+      await newTodo.save();
+    } catch (error) {
+      expect(error).toBeInstanceOf(mongoose.Error.ValidationError);
+      expect(error.errors.name).toBeDefined();
+    }
+  });
+
+  it("should fail when todo item with fields of wrong type", async () => {
+    let invalidTodo = {
+      _id: 0,
+      status: "active",
+      name: 1,
+    };
+    try {
+      const newTodo = new Todo(invalidTodo);
+      await newTodo.save();
+    } catch (error) {
+      expect(error).toBeInstanceOf(mongoose.Error.ValidationError);
+      expect(error.errors.name).toBeDefined();
+    }
+  });
+
+  it("should delete update a todo status", async () => {});
 });
